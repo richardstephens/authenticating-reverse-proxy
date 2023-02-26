95109892b74f6f988345876b65691135aa4da2d1
Richard Stephens <richard@richst.dev>
2023-02-26 20:48:46 +0100

    fetch org members on startup

--- src/github_api.rs
+++ src/github_api.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use lazy_static::lazy_static;
 use serde::Deserialize;
@@ -7,10 +7,16 @@ use tokio::sync::RwLock;
 
 lazy_static! {
     static ref USER_MAP:RwLock<HashMap<String,String>> = RwLock::new(HashMap::new());
+    static ref ORG_MEMBERS:RwLock<HashSet<String>> = RwLock::new(HashSet::new());
 }
 
 #[derive(Deserialize, Debug)]
 pub struct GhUserResponse {
+    pub login: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct GhOrgMember {
     pub login: String,
 }
 
@@ -51,7 +57,49 @@ async fn get_user_for_token(token: &String) -> Result<String, Error> {
     };
 }
 
+async fn get_org_members_page(org: &String, token: &String, page: u32) -> Vec<GhOrgMember> {
+    let mut auth_header_value: String = "Bearer ".to_string();
+    auth_header_value.push_str(token);
+    let client = reqwest::Client::new();
+    let mut req_url = "https://api.github.com/orgs/".to_string();
+    req_url.push_str(&org);
+    req_url.push_str("/members?per_page=100&page=");
+    req_url.push_str(&page.to_string());
+    println!("URL: {}", req_url);
+    let resp = client.get(&req_url)
+        .header("User-Agent", "Authenticating Reverse Proxy (https://github.com/richardstephens/authenticating-reverse-proxy)")
+        .header("Accept", "application/vnd.github+json")
+        .header("X-GitHub-Api-Version", "2022-11-28")
+        .header("Authorization", &auth_header_value)
+        .send().await;
+
+    let resp_content = resp.unwrap().json::<Vec<GhOrgMember>>().await.unwrap();
+    return resp_content;
+}
+
+pub async fn get_org_members(org: String, token: String) {
+    let mut page: u32 = 0;
+    loop {
+        let page_contents = get_org_members_page(&org, &token, page).await;
+        println!("Reading members for org: {}. Page {} got {} entries", org, page, page_contents.len());
+        let mut writeable_org_members = ORG_MEMBERS.write().await;
+        for org_member in &page_contents {
+            writeable_org_members.insert(org_member.login.clone());
+        }
+        if page_contents.len() == 0 {
+            break;
+        } else {
+            page = page + 1;
+        }
+    }
+    println!("Got {} org members", ORG_MEMBERS.read().await.len());
+}
+
 pub async fn check_token(user: String, token: String) -> bool {
+    if !ORG_MEMBERS.read().await.contains(&user) {
+        println!("user not in org");
+        return false;
+    }
     let user_details = {
         USER_MAP.read().await.get(&user).map(|x| x.clone())
     };
@@ -74,6 +122,7 @@ pub async fn check_token(user: String, token: String) -> bool {
                         writable_user_map.insert(token_username, token.clone());
                         true
                     } else {
+                        println!("Github token did not match user: {}", token_username);
                         false
                     }
                 }
--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,33 @@ use clap::Parser;
 
 use reverse_proxy::start_reverse_proxy;
 
+use crate::github_api::get_org_members;
+
 mod reverse_proxy;
 mod github_api;
 
 
-/// Simple program to greet a person
+/// Reverse proxy that uses GitHub credentials to validate that a user belongs to an org
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     /// Target URL of reverse proxy
     #[arg(short, long)]
     target: String,
+
+    /// Token to use to read organisation members
+    #[arg(long)]
+    gh_org_token: String,
+
+    /// Organisation to check membership of
+    #[arg(long)]
+    gh_org: String,
 }
 
 #[tokio::main]
 async fn main() {
     let args = Args::parse();
+    get_org_members(args.gh_org, args.gh_org_token).await;
+
     start_reverse_proxy(args.target).await
 }
